d13be6311d6752588f1827e59b3c4d65c3fd64f3
king-handy <a.king.life@pm.me>
2021-01-14 23:23:42 -0500

    Task 1 questions

--- index.js
+++ index.js
@@ -28,11 +28,17 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+
+    Code 'counter1' starts the count at 0 within the scope of the function; whereas, counter2 declares count globally.
   
   2. Which of the two uses a closure? How can you tell?
+
+    Code 'counter1' utilizes closure due to the nested function inside counterMaker() reaching outside of its local scope for a variable.
   
   3. In what scenario would the counter1 code be preferable? In what scenario would 
      counter2 be better?  
+    Code 'counter1' is prefable when incrementing values without resetting initial value. Code 'counter2' is preferable when values are reset each time code is compiled.
+
 */
 
 // counter1 code
